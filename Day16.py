f197a11bb09f72352529804028b67ee5f7f39ae0
Jacky Chan <rumbleman3@hotmail.com>
2024-12-16 02:25:38 -0800

    Day 16 complete

--- Day16.py
+++ Day16.py
@@ -26,6 +26,7 @@ import copy
 directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 routes = [[[starti, startj, [0, 1], 0]]]
 completedroutes = []
+abandonedRoutes = []
 while len(routes) > 0:
     lowestRoute = -1
     lowestIndex = -1
@@ -50,8 +51,12 @@ while len(routes) > 0:
             newscore += 1000
         if map[newi][newj] == '.':        
             if weightedmap[newi][newj] == -1 or weightedmap[newi][newj] > newscore:
-                weightedmap[newi][newj] = newscore
+                weightedmap[newi][newj] = newscore                
             else:
+                if weightedmap[newi][newj] == newscore or weightedmap[newi][newj] + 1000 == newscore:
+                    newroute = copy.deepcopy(route)
+                    newroute.append([newi, newj, newfacing, newscore])
+                    abandonedRoutes.append(newroute)
                 continue
             if len(route) == 1 or (route[len(route) - 2][0] != newi or route[len(route) - 2][1] != newj):
                 newroute = copy.deepcopy(route)
@@ -63,8 +68,43 @@ while len(routes) > 0:
             completedroutes.append(newroute)
 
 lowestscore = 0
+bestPath = []
 for route in completedroutes:
     if lowestscore == 0 or lowestscore > route[len(route) - 1][3]:
         lowestscore = route[len(route) - 1][3]
+        bestPath = route
 print(lowestscore)
+
+# Gold star
+bestPaths = []
+for stop in bestPath:
+    bestPaths.append([stop[0], stop[1]])
+print(len(bestPaths))
+
+while len(abandonedRoutes) > 0:
+    highestIndex = 0
+    highestLength = 0
+    for index in range(0, len(abandonedRoutes)):
+        route = abandonedRoutes[index]
+        length = len(route)
+        if highestLength < length:
+            highestLength = length
+            highestIndex = index
+    route = abandonedRoutes.pop(highestIndex)
+    lastStop = [route[len(route) - 1][0], route[len(route) - 1][1]]
+    if lastStop in bestPaths:
+        for routeStop in route:
+            location = [routeStop[0], routeStop[1]]
+            if location not in bestPaths:
+                bestPaths.append(location)
+
+bestpathmap = copy.deepcopy(map)
+for stop in bestPaths:
+    i = stop[0]
+    j = stop[1]
+    bestpathmap[i] = bestpathmap[i][:j] + 'O' + bestpathmap[i][j+1:]
+for line in bestpathmap:
+    print(line)
+
+print(len(bestPaths))
 
